7013ebd79e802c72b2090a6692088e92505d89fa
Gregory Vincic <g@7de.se>
2021-01-09 09:36:31 +0100

    Render api

--- generate.go
+++ generate.go
@@ -1,24 +1,31 @@
 package figo
 
 import (
+	"bytes"
+	"go/ast"
+	"go/doc"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 
 	. "github.com/gregoryv/web"
+	"github.com/gregoryv/web/toc"
 )
 
+// Generate go documentation for the given directory and its children.
 func Generate(dir, out string) error {
-	pkg, err := golist(dir)
-	if err != nil {
-		return err
-	}
-
+	docs := godoc(dir)
+	nav := Nav()
 	page := NewPage(Html(Body(
-		H1(pkg),
-		fileTree(dir),
+		nav,
+		docs,
 	)))
+	toc.MakeTOC(nav, docs, "h1", "h2", "h3")
 	return page.SaveAs(out)
 }
 
@@ -27,7 +34,68 @@ func golist(dir string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return string(out), nil
+	return strings.TrimSpace(string(out)), nil
+}
+
+func godoc(dir string) *Element {
+	w := Article()
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if skipPath(info) {
+			return filepath.SkipDir
+		}
+		if info.IsDir() {
+			dp := dir + "/" + path
+			pkgName, err := golist(dp)
+			if err != nil || pkgName == "" {
+				return filepath.SkipDir
+			}
+			s, err := docPkg(pkgName, dp)
+			if err != nil {
+				return filepath.SkipDir
+			}
+			w.With(s)
+		}
+		return nil
+	})
+	return w
+}
+
+func docPkg(pkgName, dir string) (*Element, error) {
+	files := make([]*ast.File, 0)
+	fset := token.NewFileSet()
+	gofiles, _ := filepath.Glob(dir + "/*.go")
+	for _, f := range gofiles {
+		data, _ := ioutil.ReadFile(f)
+		files = append(files, mustParse(fset, f, string(data)))
+	}
+	p, err := doc.NewFromFiles(fset, files, pkgName)
+	if err != nil {
+		return nil, err
+	}
+	s := Section(
+		H2("package ", pkgName),
+		P(p.Doc),
+	)
+	for _, f := range p.Funcs {
+		var buf bytes.Buffer
+		printer.Fprint(&buf, fset, f.Decl)
+		s.With(
+			H3(buf.String()),
+			P(f.Doc),
+		)
+	}
+	return s, nil
+}
+
+func mustParse(fset *token.FileSet, filename, src string) *ast.File {
+	f, err := parser.ParseFile(fset, filename, src, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
 
 func fileTree(dir string) *Element {
@@ -39,7 +107,9 @@ func fileTree(dir string) *Element {
 		if skipPath(info) {
 			return filepath.SkipDir
 		}
-		ul.With(Li(path))
+		if !info.IsDir() {
+			ul.With(Li(path))
+		}
 		return nil
 	})
 	return ul
